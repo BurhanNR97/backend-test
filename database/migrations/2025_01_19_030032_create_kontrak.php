de7d4aad6192a86da9fd4878b8343c39d27bb44a
Burhan Nasir Rauf, S.Kom. <mr.bnr97@gmail.com>
2025-01-19 13:09:45 +0800

    Update 2025_01_19_030032_create_kontrak.php

--- database/migrations/2025_01_19_030032_create_kontrak.php
+++ database/migrations/2025_01_19_030032_create_kontrak.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->date('start_date');
             $table->date('end_date');
             $table->timestamps();
+
+            $table->foreign('employee_id')->references('nik')->on('kontrak');
         });
     }
 
